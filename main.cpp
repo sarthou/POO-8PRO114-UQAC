fe887fec0074017a73f0187db31f53622bbc7741
guillaume <guillaum324bis@hotmail.fr>
2016-11-16 17:26:30 -0500

    Management of exceptions

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/throw_exception.hpp>
 #include "Utility\export.h"
 
 #define BOOST_ALL_NO_LIB
@@ -16,22 +17,34 @@ int main()
 {
 	Ligue_hokey ligue;
 
+	std::ifstream ifs("fichierDeSerialisation.txt");
+	try
 	{
-		std::ifstream ifs("fichierDeSerialisation.txt");
-		if (ifs.is_open())
-		{
-			boost::archive::text_iarchive ia(ifs);
-			ia >> ligue;
-		}
+		boost::archive::text_iarchive ia(ifs);
+		ia >> ligue;
+	}
+	catch (boost::archive::archive_exception e)
+	{
+		std::cerr << "Erreur lors de l'ouverture du fichier de serialisation ou lors de la deserialization\n";
+		std::cerr << "Une application vide est donc chargee\n";
+		system("PAUSE");
 	}
 	
 	ligue.run();
 
-	system("PAUSE");
-
 	std::ofstream ofs("fichierDeSerialisation.txt");
-	boost::archive::text_oarchive oa(ofs);
-	oa << ligue;
+	try
+	{
+		boost::archive::text_oarchive oa(ofs);
+		oa << ligue;
+	}
+	catch (boost::archive::archive_exception e)
+	{
+		std::cerr << "Erreur lors de la création du fichier de serialisation ou lors de la serialization\n";
+		std::cerr << "Une application vide est donc chargee à la prochaine execution\n";
+	}
+
+	system("PAUSE");
 
 	return 0;
 }
